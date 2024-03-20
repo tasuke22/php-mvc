fc28cda0b3402dc2087fb5a0da0291a514b851a6
tasuke22 <tasukesuke@gmail.com>
2024-03-21 02:50:54 +0900

    アンチパターン：クエリパラメーターでコントローラーのアクションを分ける

--- src/index.php
+++ src/index.php
@@ -3,4 +3,12 @@
 require 'controllers/todos.php';
 
 $controller = new Todos();
+
+$action = $_GET["action"];
+
+if ($action === "index") {
+    $controller->index();
+} elseif ($action === "show") {
+    $controller->show();
+}
 $controller->show();
