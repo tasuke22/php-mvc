2473f904782ba12718cafc4f3a7821a63274084b
tasuke22 <tasukesuke@gmail.com>
2024-03-21 03:02:44 +0900

    リファクタ：クエリパラメーターからのコントローラーを動的に読み込んで対応するアクションも動的に生成する

--- src/index.php
+++ src/index.php
@@ -4,10 +4,8 @@
 $action = $_GET["action"];
 $controller = $_GET["controller"];
 
-if ($controller === "todos") {
-    require 'controllers/todos.php';
-    $controller_object = new Todos();
-} elseif ($controller === "home") {
-    require 'controllers/home.php';
-    $controller_object = new Home();
-}
+require "controllers/$controller.php";
+
+$controller_object = new $controller;
+
+$controller_object->$action();
