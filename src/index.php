1ed8aabea9a2cad6ae300c3beef70a19f190e0df
tasuke22 <tasukesuke@gmail.com>
2024-03-23 09:04:36 +0900

    不正なURLの場合は例外を投げる

--- src/index.php
+++ src/index.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
+if ($path === false) {
+    throw new UnexpectedValueException("Malformed URL: '$_SERVER[REQUEST_URI]'");
+}
+
 spl_autoload_register(function (string $class_name) {
     require str_replace("\\", "/", $class_name) . ".php";
 });
