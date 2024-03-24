886f1bd4d2c2b8f7d90d4f9d4c2fde9ddf3cc4be
tasuke22 <tasukesuke@gmail.com>
2024-03-24 15:29:17 +0900

    共通のコードをフレームワークモデルクラスに抽出する

--- src/App/Models/Todo.php
+++ src/App/Models/Todo.php
@@ -10,24 +10,13 @@ class Todo extends Model
 {
     protected $table = 'todos';
 
-    protected array $errors = [];
 
-    protected function addError(string $field, string $message): void
-    {
-        $this->errors[$field] = $message;
-    }
-
-    protected function validate(array $data): bool
+    protected function validate(array $data): void
     {
         if (empty($data["title"])) {
             $this->addError("title", "Title is required");
         }
-        return empty($this->errors);
     }
 
-    public function getErrors(): array
-    {
-        return $this->errors;
-    }
 }
 
