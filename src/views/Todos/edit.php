42562d320c0bac8b286f3fd3ad1bc4659e6d6096
tasuke22 <tasukesuke@gmail.com>
2024-03-24 17:51:57 +0900

    レコードの既存の属性を含むフォームを表示する

--- src/views/Todos/edit.php
+++ src/views/Todos/edit.php
@@ -1,5 +1,9 @@
 <h1>Edit Todo</h1>
 
+<form method="post" action="/todos/<?= $todo["id"] ?>/update">
+    <?php require "form.php"; ?>
+</form>
+
 <p>
   <a href="/todos/<?= $todo["id"] ?>/show">Cancel</a>
 </p>
--- src/views/Todos/new.php
+++ src/views/Todos/new.php
@@ -2,22 +2,6 @@
 
 <form method="post" action="/todos/create">
 
-  <lable for="title">Title</lable>
-  <input type="text" name="title" id="title">
-
-  <?php if (isset($errors['title'])): ?>
-    <p><?= $errors['title'] ?></p>
-  <?php endif; ?>
-
-  <label for="description">Description</label>
-  <textarea name="description" id="description"></textarea>
-
-  <lable for="completed">Completed</lable>
-  <input value="0" name="completed" id="completed">
-
-  <lable for="user_id">UserId</lable>
-  <input value="860c602c-2b99-48b3-82b7-253f52ad9c7e" name="user_id" id="user_id">
-
-  <button>Save</button>
+  <?php require "form.php"; ?>
 
 </form>
