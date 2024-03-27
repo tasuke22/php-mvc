7f28d6e0b8f1375d8ab787f07ca813d449ca0670
tasuke22 <tasukesuke@gmail.com>
2024-03-28 01:14:30 +0900

    リクエストの処理を担当するためのインターフェースを定義する

--- src/App/Middleware/ChangeResponseExample.php
+++ src/App/Middleware/ChangeResponseExample.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 namespace app\App\Middleware;
 
 use Framework\Request;
+use Framework\RequestHandlerInterface;
 use Framework\Response;
 
 class ChangeResponseExample
 {
-    public function process(Request $request, $next): Response
+    public function process(Request $request, RequestHandlerInterface $next): Response
     {
         $response = $next->handle($request);
         $response->setBody($response->getBody() . " hello from the middleware");
