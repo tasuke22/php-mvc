ba2f05ab083265ca872eb3aaf83b0a4e32ba6ef6
tasuke22 <tasukesuke@gmail.com>
2024-03-28 01:11:05 +0900

    レスポンスを変更するミドルウェアクラスを追加する

---
+++ src/App/Middleware/ChangeResponseExample.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\App\Middleware;
+
+use Framework\Request;
+use Framework\Response;
+
+class ChangeResponseExample
+{
+    public function process(Request $request, $next): Response
+    {
+        $response = $next->handle($request);
+        $response->setBody($response->getBody() . " hello from the middleware");
+        return $response;
+    }
+}
